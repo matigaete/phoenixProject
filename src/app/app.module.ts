ac65ff4e9b8d4109b4960c0f2048da85c6631bf8
LeeSerker <camilitoruizsmith@gmail.com>
2020-08-11 23:24:15 -0400

    Initial Commit
    Se carga proyecto con carpetas iniciales.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './include/footer/footer.component';
+import { HeaderComponent } from './include/header/header.component';
+import { IndexComponent } from './Home/index/index.component';
+import { CreateComponent } from './Mantenedores/Productos/create/create.component';
+import { SearchComponent } from './Mantenedores/Productos/search/search.component';
+import { ModifyComponent } from './Mantenedores/Productos/modify/modify.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    IndexComponent,
+    CreateComponent,
+    SearchComponent,
+    ModifyComponent
   ],
   imports: [
     BrowserModule,
