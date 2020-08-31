da514ffbaf8a02d0657910f0edfb9e3091cda5d1
matigaete <38695856+matigaete@users.noreply.github.com>
2020-08-31 16:47:41 -0400

    HTTP commit
    Adición de componentes http para conexión a servidor local con php

--- src/app/Mantenedores/Productos/index-productos/index-productos.component.ts
+++ src/app/Mantenedores/Productos/index-productos/index-productos.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';   
 import { BusinessService } from '../../../business.service';
-import { Ilista } from '../../../Interfaces/ilista';
+import { Ilista } from '../../../Interfaces/ilista'; 
+import { Producto } from 'src/app/Clases/producto';
 @Component({
   selector: 'app-index-productos',
-  template:  `<nav aria-label="Page navigation example">
+  template:  `<nav>
                 <ul class="pagination justify-content-center">
                   <li *ngFor="let a of acciones" class="page-item {{a.current}}" role="button">  
                     <a class="page-link" (click)="asignarOpcion(a.id)">
@@ -14,8 +15,8 @@ import { Ilista } from '../../../Interfaces/ilista';
               </nav>
               <div class="container">  
                 <app-create  *ngIf="option == 1" [isNew]="option"></app-create>
-                <app-search  *ngIf="option == 2" [valor]="option"></app-search>
-                <app-search  *ngIf="option == 3" [valor]="option"></app-search> 
+                <app-search  *ngIf="option == 2" [iValor]="option"></app-search>
+                <app-search  *ngIf="option == 3" [iValor]="option"></app-search> 
                 <app-listado *ngIf="option == 4"></app-listado> 
               </div>`,
   styles: []
@@ -24,18 +25,31 @@ export class IndexProductosComponent implements OnInit {
   
   public acciones : Ilista[];
   public option : number;
-  public current : string;
+  public current : string; 
+  public producto : Producto;
   
   constructor( private businessService : BusinessService ) { }
     
-  ngOnInit(): void {
-      this.acciones = this.businessService.getAcciones();
-      this.option   = this.businessService.getOption();
-      this.acciones[this.option - 1].current = this.businessService.getActive();
+  public ngOnInit(): void {
+    this.acciones = this.businessService.getAcciones();
+    this.option   = this.businessService.getOption();
+    this.acciones[this.option - 1].current = this.businessService.getActive();
+    this.seleccionar();
   }
+
+  public ngDoCheck(): void { 
+    this.option   = this.businessService.getOption();
+    this.acciones[this.option - 1].current = this.businessService.getActive();
+}
   
   public asignarOpcion(option : number) : void {
     this.acciones = this.businessService.asignarOpcion(option, this.acciones);
     this.option   = this.businessService.getOption();
   }
+
+  public seleccionar(){ 
+      this.businessService.seleccionar(1232154).subscribe(result => this.producto = result[0]);
+      console.log(this.producto);
+  }
+
 }
--- src/app/include/info/info.component.ts
+++ src/app/include/info/info.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { BusinessService } from 'src/app/business.service';
-import { Producto } from 'src/app/Clases/producto';
+import { Producto } from 'src/app/Clases/producto'; 
 
 @Component({
   selector: 'app-info',
@@ -15,25 +15,27 @@ import { Producto } from 'src/app/Clases/producto';
                   <p class="card-text">Stock crítico: {{stockCritico}}</p>
                   <p class="card-text">Último precio de compra: {{precioCompra}}</p>
                   <p class="card-text">Precio de venta: {{precioVenta}}</p>
-                  <a class="btn btn-primary">{{modificar}}</a>
+                  <a class="btn btn-primary {{disabled}}" (click)="asignarOpcion(2)">{{modificar}}</a>
                 </div>
               </div>`,
   styles: []
 })
 export class InfoComponent implements OnInit {
 
-  @Input() producto : Producto;
+  @Output() oValor = new EventEmitter<number>(); 
+  @Input() iProducto : Producto;
   public nombre : string; 
   public info : string; 
   public modificar : string; 
   public stock : number;
   public stockCritico : number;
   public precioCompra : number;
-  public precioVenta : number;
+  public precioVenta : number; 
+  public disabled : string;
 
   constructor( private businessService : BusinessService ) { }
 
-  public ngOnInit(): void {
+  public ngOnInit(): void { 
     this.nombre = this.businessService.getNombre();
     this.modificar = this.businessService.getAcciones()[1].nombre; 
     this.info = this.businessService.getInfo();
@@ -44,15 +46,23 @@ export class InfoComponent implements OnInit {
   }
 
   public ngDoCheck(): void{ 
-    if (this.producto !== undefined) {
-      var product = this.producto[0];
+    if (this.iProducto !== undefined) {
+      var product = this.iProducto[0];
       this.nombre = product.getNombre(); 
       this.info = product.getDescripcion();
       this.stock = product.getStock();
       this.stockCritico = product.getStockCritico();
       this.precioCompra = product.getPrecioCompra();
       this.precioVenta = product.getPrecioVenta();
+      this.disabled = null;
     }
+    else{
+      this.disabled = this.businessService.getDisabled();
+    }
+  }
+
+  public asignarOpcion(option : number) : void {
+    this.oValor.emit(option); 
   }
 
 }
