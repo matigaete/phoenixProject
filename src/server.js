6befd16cc884912da00e921d76da291e8a5d3576
matigaete <matias.gaetep@sansano.usm.cl>
2022-04-07 19:02:59 -0400

    ahora si

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname+'/phoenixProject'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/phoenixProject/index.html'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/phoenixProject'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/phoenixProject/index.html'));
 });
 
+// default Heroku port
 app.listen(process.env.PORT || 5000);
